a0ded3d0622bf9ccd172a18e4bf832a8bc92d4c0
dogametal <dogametal@hotmail.com>
2020-12-15 05:41:50 -0300

    Heal Attack

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,7 @@
 const ATTACK_VALUE = 10;
 const MONSTER_ATTACK_VALUE = 14;
 const STRONG_ATTACK_VALUE = 17;
+const HEAL_VALUE = 20;
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
@@ -12,9 +13,12 @@ function handlerAttack(mode) {
     let typeMode = mode;
     const damage = dealMonsterDamage(typeMode);
     currentMonsterHealth -= damage;
+    endRound();
+}
+
+function endRound() {
     const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
     currentPlayerHealth -= playerDamage;
-
 
     if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
         alert("You won !");
@@ -23,7 +27,6 @@ function handlerAttack(mode) {
     } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
         alert("You have a draw !");
     }
-
 }
 
 function attackHandler() {    // Start system button Attack
@@ -34,5 +37,21 @@ function strongAttackHandler() {
     handlerAttack(STRONG_ATTACK_VALUE)
 }
 
+function healPlayerHandler() {
+    let healValue;
+    if (currentPlayerHealth >= chosenMaxLife - HEAL_VALUE) {
+        alert("You can´t health more than your max initial health.")
+        healValue = chosenMaxLife - currentPlayerHealth;
+    } else {
+        healValue = HEAL_VALUE;
+    }
+        
+
+    increasePlayerHealth(healValue);
+    currentPlayerHealth += healValue;
+    endRound();
+}
+
 attackBtn.addEventListener('click', attackHandler);
 strongAttackBtn.addEventListener('click', strongAttackHandler);
+healBtn.addEventListener('click', healPlayerHandler);
